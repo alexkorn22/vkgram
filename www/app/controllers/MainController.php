112f8cbe350cae8775474f3e2c2cceed280cd774
Aleksandr <a@test.com>
2017-05-30 20:57:36 +0300

    Init microfm

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -11,25 +11,6 @@ use vendor\core\User;
 class MainController extends AppController{
 
     public function indexAction() {
-        View::setMeta('Главная', 'Описание');
-        $posts = App::$app->cache->get('posts');
-        if (false === $posts) {
-            $posts = \R::findAll('posts');
-            App::$app->cache->set('posts', $posts);
-        }
-        $this->setVars(compact('posts'));
-    }
-
-    public function testAjaxAction() {
-        if ($this->isAjax()) {
-            $post = \R::findOne('posts',"id = {$_POST['id']}");
-            $this->loadView('testAjax', compact('post'));
-            die();
-        }
-    }
-
-    public function testAction() {
 
     }
-
 }
--- www/app/views/Main/testAjax.php
+++
@@ -1,8 +0,0 @@
-<div class="panel panel-success">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?=$post['title']?></h3>
-    </div>
-    <div class="panel-body">
-        <?=$post['text']?>
-    </div>
-</div>
