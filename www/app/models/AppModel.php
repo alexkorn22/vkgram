3130597ecfe81c8eb4bdf843055bbb5367dc784b
Aleksandr <a@test.com>
2017-06-08 19:56:53 +0300

    Изменена абстрактная модель

--- www/app/models/AppModel.php
+++ www/app/models/AppModel.php
@@ -7,5 +7,36 @@ use vendor\core\base\Model;
 class AppModel extends Model {
 
     protected $id = 0;
+    protected $fields = [];
+
+    public function __get($name){
+       return $this->fields[$name];
+    }
+
+    public function __set($name, $value){
+        $this->fields[$name] = $value;
+    }
+
+    public function fillFields($data) {
+        foreach ($this->fields as $key => $value) {
+            $this->fields[$key] = $data[$key];
+        }
+    }
+
+    public function fillRecordFields($record) {
+        foreach ($this->fields as $key => $value) {
+            $record->$key = $value;
+        }
+    }
+
+    public function save() {
+        if ($this->id){
+            $record = \R::load(self::$table,$this->id);
+        } else {
+            $record = \R::dispense(self::$table);
+        };
+        $this->fillRecordFields($record);
+        $this->id = \R::store($record);
+    }
 
 }
